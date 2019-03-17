084cbba4950db49c52e1fb46c4b3bb28561e5d51
ssilangoss <ilango.sel87@gmail.com>
2019-03-17 16:32:43 +0530

    second commit

---
+++ src/test/java/sampleGitTest/SampleGitTest.java
@@ -0,0 +1,24 @@
+package sampleGitTest;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class SampleGitTest {
+
+	@Test
+	public void loginTest() throws IOException{
+	WebDriver wd = new FirefoxDriver();
+	wd.manage().window().maximize();
+	wd.get("https://www.google.com/");
+	
+	File f = ((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+	FileUtils.copyFile(f, new File("C:\\SeleniumWorkspace\\sampleGit\\Screenshot\\image.jpeg"));
+	}
+}
